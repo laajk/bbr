12beea4d741806ce7d16732022aa4df4694187b8
laajk <51731191+laajk@users.noreply.github.com>
2021-09-11 09:12:17 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install docker-ce docker-ce-cli -y
 service docker start
+systemctl enable docker
 docker pull shadowsocks/shadowsocks-libev
 echo docker run -e PASSWORD=9MLSpPmNt -p 8388:8388 -p 8388:8388/udp -d --restart always shadowsocks/shadowsocks-libev
